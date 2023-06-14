4be1e5955f0147644e6fb09b811869dd055191f8
AAndreazza <alanaandreazza26@hotmail.com>
2023-06-14 14:54:06 -0300

    alteracao frase

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -9,7 +9,7 @@ const Team = () => {
         <div class="mx-auto max-w-7xl px-6 text-center lg:px-8">
             <div class="mx-auto max-w-2xl">
             <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Conheça nosso time</h2>
-            <p class="mt-4 text-lg leading-8 text-gray-600">Somos um grupo de indivíduos apaixonados pelo que fazemos.</p>
+            <p class="mt-4 text-lg leading-8 text-gray-600">Somos um trio de mulheres apaixonadas pelo que fazemos.</p>
             </div>
             <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             <li>
