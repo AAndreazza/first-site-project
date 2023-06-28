1822c884b9f53913f2f78d4a464b6ddd7dee1583
AAndreazza <alanaandreazza26@hotmail.com>
2023-06-28 16:08:19 -0300

    arrumando erro

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -12,23 +12,23 @@ const Team = () => {
             <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl' id='time'>Conheça nosso time</h2>
             <p className='mt-4 text-lg leading-8 text-gray-600'>Somos um trio de mulheres apaixonadas pelo que fazemos.</p>
             </div>
-            <ul role='list' className='mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
-            <li>
+            <div role='list' className='mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+            <div>
                 <StaticImage className='mx-auto h-56 w-56 rounded-full' src="../../images/fotoLuanna.jpg" alt=''/>
                 <h3 className='mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900'>Luanna Catarina</h3>
                 <p className='text-sm leading-6 text-gray-600'>Fisioterapeuta</p>
-            </li>
-            <li>
+            </div>
+            <div>
                 <StaticImage className='mx-auto h-56 w-56 rounded-full' src="../../images/fotoMaria.jpg" alt=''/>
                 <h3 className='mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900'>Maria Luiza</h3>
                 <p className='text-sm leading-6 text-gray-600'>Esteticista</p>
-            </li>
-            <li>
+            </div>
+            <div>
                 <StaticImage className='mx-auto h-56 w-56 rounded-full' src="../../images/fotoAlana.jpg" alt=''/>
                 <h3 className='mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900'>Alana Cristina</h3>
                 <p className='text-sm leading-6 text-gray-600'>Massagista</p>
-            </li>
-            </ul>
+            </div>
+            </div>
         </div>
         </div>
 
